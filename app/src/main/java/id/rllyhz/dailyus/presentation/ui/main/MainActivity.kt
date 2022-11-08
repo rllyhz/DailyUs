d3b126b6f09069f2f5af33cdd61c1daf2f48d819
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-08 07:50:22 +0700

    Set main UI

---
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.rllyhz.dailyus.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.rllyhz.dailyus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
