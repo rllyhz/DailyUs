2a8a5bd8fe56b62bf9d89a328bd837261616f022
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-09 02:29:17 +0700

    Created local model files

--- app/src/main/java/id/rllyhz/dailyus/data/source/local/model/StoryEntity.kt
+++ app/src/main/java/id/rllyhz/dailyus/data/source/local/model/StoryEntity.kt
@@ -1,3 +1,30 @@
 package id.rllyhz.dailyus.data.source.local.model
 
-data class StoryEntity()
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = "stories")
+data class StoryEntity(
+    @PrimaryKey
+    val id: String,
+
+    val name: String,
+
+    val description: String,
+
+    val photoUrl: String,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: String,
+
+    @ColumnInfo(name = "lat")
+    val latitude: Double?,
+
+    @ColumnInfo(name = "lon")
+    val longitude: Double?
+
+) : Parcelable
