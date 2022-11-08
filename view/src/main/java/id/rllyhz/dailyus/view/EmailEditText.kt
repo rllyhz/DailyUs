a24cdb702945c67c8cd42e63755318a2c4239a60
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-09 05:08:27 +0700

    Created custom view for email and password EditText with type of AppCompatEditText class

---
+++ view/src/main/java/id/rllyhz/dailyus/view/EmailEditText.kt
@@ -0,0 +1,55 @@
+package id.rllyhz.dailyus.view
+
+import android.content.Context
+import android.os.Build
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+
+class EmailEditText : AppCompatEditText {
+    constructor(context: Context) : super(context) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initView()
+    }
+
+    private var errorMessage: String? = null
+
+    fun setErrorMessage(message: String) {
+        errorMessage = message
+    }
+
+    private fun initView() {
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        compoundDrawablePadding = 10
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setAutofillHints(AUTOFILL_HINT_EMAIL_ADDRESS)
+        }
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                error =
+                    if (!text.isNullOrEmpty() && !Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                        errorMessage
+                    } else null
+            }
+        })
+    }
+}
