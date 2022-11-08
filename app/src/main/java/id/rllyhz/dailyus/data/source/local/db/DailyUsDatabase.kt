4aa1fd20be8d5ed5fd747cbcebb1e285f2164c4c
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-09 02:38:30 +0700

    Created local db related files

--- app/src/main/java/id/rllyhz/dailyus/data/source/local/db/DailyUsDatabase.kt
+++ app/src/main/java/id/rllyhz/dailyus/data/source/local/db/DailyUsDatabase.kt
@@ -1,4 +1,17 @@
 package id.rllyhz.dailyus.data.source.local.db
 
-class DailyUsDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import id.rllyhz.dailyus.data.source.local.model.StoryEntity
+import id.rllyhz.dailyus.data.source.local.model.StoryKeys
+
+@Database(
+    entities = [StoryEntity::class, StoryKeys::class],
+    version = 1,
+    exportSchema = false,
+)
+abstract class DailyUsDatabase : RoomDatabase() {
+
+    abstract fun storiesDao(): StoriesDao
+    abstract fun storyKeysDao(): StoryKeysDao
 }
