4b49748023ca20a84c0f5514e09a5ff8c99efa83
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-14 04:40:04 +0700

    Updated register UI

--- app/src/main/java/id/rllyhz/dailyus/presentation/ui/auth/RegisterFragment.kt
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/auth/RegisterFragment.kt
@@ -1,12 +1,16 @@
 package id.rllyhz.dailyus.presentation.ui.auth
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import id.rllyhz.dailyus.R
 import id.rllyhz.dailyus.databinding.FragmentRegisterBinding
 
 @AndroidEntryPoint
@@ -21,6 +25,28 @@ class RegisterFragment : Fragment() {
     ): View? {
         binding = FragmentRegisterBinding.inflate(inflater)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.run {
+            layoutTopBar.topBarTvTitle.text = getString(R.string.title_login)
+            layoutTopBar.topBarBtnBack.setOnClickListener { findNavController().navigateUp() }
+
+            registerEtEmail.setErrorMessage(getString(R.string.email_invalid_message))
+            registerEtPassword.setErrorMessage(getString(R.string.password_invalid_message))
+
+            val submitBtn =
+                ObjectAnimator.ofFloat(registerBtnSubmit, View.ALPHA, 1f)
+                    .setDuration(1000).apply {
+                        startDelay = 150
+                    }
+
+            AnimatorSet().apply {
+                play(submitBtn)
+            }.start()
+        }
     }
 
     override fun onDestroyView() {
