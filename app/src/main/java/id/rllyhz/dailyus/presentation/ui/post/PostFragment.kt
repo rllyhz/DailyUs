f2361165e21cc204a49e55890a31fb6bb91077f7
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-15 02:03:38 +0700

    Updated main UI

--- app/src/main/java/id/rllyhz/dailyus/presentation/ui/post/PostFragment.kt
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/post/PostFragment.kt
@@ -1,5 +1,25 @@
 package id.rllyhz.dailyus.presentation.ui.post
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.rllyhz.dailyus.databinding.FragmentPostBinding
 
-class PostFragment : Fragment()
+class PostFragment : Fragment() {
+    private var binding: FragmentPostBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPostBinding.inflate(inflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/id/rllyhz/dailyus/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/profile/ProfileFragment.kt
@@ -1,5 +1,25 @@
 package id.rllyhz.dailyus.presentation.ui.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.rllyhz.dailyus.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment()
+class ProfileFragment : Fragment() {
+    private var binding: FragmentProfileBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
