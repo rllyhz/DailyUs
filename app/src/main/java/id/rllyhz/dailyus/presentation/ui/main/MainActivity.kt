8af5f2889ade7f1c5f0afe09c71fff7176da13a4
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-15 02:17:19 +0700

    Set bottom menu and navigation on main UI

--- app/src/main/java/id/rllyhz/dailyus/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/main/MainActivity.kt
@@ -1,12 +1,24 @@
 package id.rllyhz.dailyus.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import id.rllyhz.dailyus.R
+import id.rllyhz.dailyus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavMain.setupWithNavController(navController)
     }
 }
