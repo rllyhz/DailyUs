8816b0f9743e8b6665114ba433b0cb4a3ed6ecd9
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-11-17 07:17:19 +0700

    Finished profile UI of main UI

--- app/src/main/java/id/rllyhz/dailyus/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/id/rllyhz/dailyus/presentation/ui/profile/ProfileFragment.kt
@@ -1,8 +1,8 @@
 package id.rllyhz.dailyus.presentation.ui.profile
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +18,8 @@ import id.rllyhz.dailyus.presentation.ui.main.MainViewModel
 class ProfileFragment : Fragment() {
     private var binding: FragmentProfileBinding? = null
     private val viewModel: MainViewModel by activityViewModels()
+
+    private val animDuration = 1000L
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,28 +44,16 @@ class ProfileFragment : Fragment() {
                 profileTvEmail.text = it
             }
 
+            profileBtnChangeLanguage.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+            }
+
             profileBtnLogout.setOnClickListener {
                 viewModel.logout()
 
                 requireActivity().finish()
                 findNavController().navigate(R.id.action_profileFragment_to_authActivity)
             }
-
-            val progressBarAnim = ObjectAnimator.ofFloat(profileProgressbar, View.ALPHA, 0f)
-                .setDuration(500).apply { startDelay = 400 }
-            val tvFullNameAnim =
-                ObjectAnimator.ofFloat(profileTvFullName, View.ALPHA, 1f)
-                    .setDuration(1000).apply { startDelay = 400 }
-            val tvEmailAnim =
-                ObjectAnimator.ofFloat(profileTvEmail, View.ALPHA, 1f).setDuration(1000)
-                    .apply { startDelay = 800 }
-            val btnLogoutAnim =
-                ObjectAnimator.ofFloat(profileBtnLogout, View.ALPHA, 1f).setDuration(1000)
-                    .apply { startDelay = 1200 }
-
-            AnimatorSet().apply {
-                playTogether(progressBarAnim, tvFullNameAnim, tvEmailAnim, btnLogoutAnim)
-            }.start()
         }
     }
 }
